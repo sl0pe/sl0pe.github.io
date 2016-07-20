a6b4077065f9e4418c4f98c90c1c51c160b9ef3a
helenamerk <helena.lmtm@gmail.com>
2016-07-20 14:13:08 -0700

    Update makerequests.go

--- makerequests.go
+++ makerequests.go
@@ -10,6 +10,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/request", handler)
     http.ListenAndServe(":8080", nil)
 }
